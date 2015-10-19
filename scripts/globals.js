a77838c0ec1c1feb31db6902039b9b3483349696
Jim Tsai <jim.h.tsai@gmail.com>
2015-10-19 10:27:09 +0800

    another long awaited commit

--- scripts/globals.js
+++ scripts/globals.js
@@ -8,15 +8,36 @@ qwest.get('../api-manifest/items.json')
 
 qwest.get('../api-manifest/talent.json')
 .then(function(xhr, res){
-	DIM.talent = JSON.parse(res);
+	DIM.talents = JSON.parse(res);
 	console.log("talent ready");
 });
 
 qwest.get('../api-manifest/buckets.json')
 .then(function(xhr, res){
-	DIM.talent = JSON.parse(res);
+	DIM.buckets = JSON.parse(res);
 	console.log("bucket ready");
 });
+
+qwest.get('../api-manifest/stats.json')
+.then(function(xhr, res){
+	DIM.stats = JSON.parse(res);
+	console.log("stats ready");
+});
+
+DIM.inventory = {};
+DIM.ignoredStats = ["Inventory Size", "Light"];
+
+DIM.damageType = {};
+DIM.damageType[0] = "None";
+DIM.damageType[1] = "Kinetic";
+DIM.damageType[2] = "Arc";
+DIM.damageType[3] = "Solar";
+DIM.damageType[4] = "Void";
+
+DIM.inventoryFilter = ["Shader", "Engram", "Subclass", "Vehicle",
+										 	"Ship", "Material", "Consum", "Emblem", "Emote", "Default"
+											];
+
 
 DIM.categoryMapping = {};
 DIM.categoryMapping["scout-rifle"] = "Scout Rifle";
